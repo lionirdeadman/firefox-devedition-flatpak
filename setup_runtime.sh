e901a3f70d35f09cafadf0bf8cf84a2d3c7bf4a6
lionirdeadman <lionirdeadman@lionirdeadman.pc>
2019-03-31 00:01:43 -0400

    Update to 3.32 and 18.08 and hope nothing breaks

--- setup_runtime.sh
+++ setup_runtime.sh
@@ -3,5 +3,5 @@ set -ux
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-flatpak install flathub org.gnome.Platform//3.28 org.gnome.Sdk//3.28
-flatpak install flathub org.freedesktop.Sdk.Extension.rust-stable//1.6
+flatpak install flathub org.gnome.Platform//3.32 org.gnome.Sdk//3.32
+flatpak install flathub org.freedesktop.Sdk.Extension.rust-stable//18.08
